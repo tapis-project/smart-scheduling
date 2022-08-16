20e70b28997326c0ca796becd63323b304696f63
Costaki Skevofilax <cskevofilax@utexas.edu>
2022-08-16 16:39:58 -0500

    Working script - converts time to UTC time (Need to see what time zone the local time is for the data) and converts timelimit column to a minutes format based around defensive coding stradegy

--- test-search.py
+++ test-search.py
@@ -1,18 +1,25 @@
 from __future__ import print_function
+from datetime import datetime
+import mysql.connector
+from mysql.connector import errorcode
+import os
+import re
+import pytz
 import logging
 from typing import List
 import socket
 import math
 import shutil
-from datetime import datetime
-import mysql.connector
-from mysql.connector import errorcode
-import os
 
-def isConnected(connection):
-    print('Successfully Connected to SQL Database')
+def connect():
 
-def createIfNotExists(connection, cursor, tableName):
+    connection = mysql.connector.connect(host="127.0.0.1", user='costaki', passwd='password', database="HPC_Job_Time_Data")
+
+    print('\nSuccessfully Connected to SQL Database')
+
+    return connection
+
+def createIfNotExists(connection, tableName):
 
     '''
 
@@ -24,40 +31,156 @@ def createIfNotExists(connection, cursor, tableName):
     '''
 
     connection.get_warnings = True
-    cursor.execute("CREATE TABLE IF NOT EXISTS " + tableName + " (jobid int unsigned NOT NULL PRIMARY KEY, user varchar(80) NOT NULL, account varchar(50) NOT NULL, start datetime NOT NULL, end datetime NOT NULL, submit datetime NOT NULL, queue TEXT NOT NULL, timelimit TEXT NOT NULL, jobname TEXT NOT NULL, state TEXT NOT NULL, nnodes int unsigned NOT NULL, reqcpus int unsigned NOT NULL, nodelist TEXT NOT NULL)")
+    cursor = connection.cursor()
+
+    cursor.execute("CREATE TABLE IF NOT EXISTS " + tableName + " (jobid int unsigned NOT NULL PRIMARY KEY, user varchar(80) NOT NULL, account varchar(50) NOT NULL, start datetime NOT NULL, end datetime NOT NULL, submit datetime NOT NULL, queue varchar(22) NOT NULL, timelimit int unsigned NOT NULL, jobname varchar(50) NOT NULL, state varchar(20) NOT NULL, nnodes int unsigned NOT NULL, reqcpus int unsigned NOT NULL, nodelist TEXT NOT NULL)")
     tuple = cursor.fetchwarnings() # <- returns a list of tuples
+
     if tuple is None:
         print('New table generated\nCreating indexes')
         # ALl permissions granted, no warning message or message in general outputted to the user, no need for conditional statements
-        cursor.execute('CREATE INDEX datatimesort ON HPC_Job_Time_Data.' + tableName + '(submit, start, end)')
-        cursor.execute('CREATE INDEX nodesort ON HPC_Job_Time_Data.' + tableName + '(nnodes, reqcpus)')
+
+        # Seperate based on each index
+
+        cursor.execute('CREATE INDEX index_jobid ON HPC_Job_Time_Data.' + tableName + '(jobid)')
+        cursor.execute('CREATE INDEX index_user ON HPC_Job_Time_Data.' + tableName + '(user)')
+        cursor.execute('CREATE INDEX index_account ON HPC_Job_Time_Data.' + tableName + '(account)')
+
+        cursor.execute('CREATE INDEX index_submit ON HPC_Job_Time_Data.' + tableName + '(submit)')
+        cursor.execute('CREATE INDEX index_start ON HPC_Job_Time_Data.' + tableName + '(start)')
+        cursor.execute('CREATE INDEX index_end ON HPC_Job_Time_Data.' + tableName + '(end)')
+
+        cursor.execute('CREATE INDEX index_queue ON HPC_Job_Time_Data.' + tableName + '(queue)')
+        cursor.execute('CREATE INDEX index_timelimit ON HPC_Job_Time_Data.' + tableName + '(timelimit)')
+        cursor.execute('CREATE INDEX index_state ON HPC_Job_Time_Data.' + tableName + '(state)')
+
+        cursor.execute('CREATE INDEX index_nnodes ON HPC_Job_Time_Data.' + tableName + '(nnodes)')
+        cursor.execute('CREATE INDEX index_reqcpus ON HPC_Job_Time_Data.' + tableName + '(reqcpus)')
+        #cursor.execute('CREATE INDEX index_nodelist ON HPC_Job_Time_Data.' + tableName + '(nodelist)')
+
         connection.commit() # Commits any tables and indices that were created to the database
 
     elif(tuple[0][1] == 1050):
-        print('Table already exists, error code was raised successfully')
+        print('\nTable already exists')
 
-def injection(connection, cursor, tableName):
+    cursor.close()
+
+def injection(connection, tableName):
 #Injects data into a specified table
-    source = '/home/ubuntu/jobs_data/' + tableName + '/'
-    archive = source + 'Archive/'
+
+    cursor = connection.cursor()
+
+    source = '/home/ubuntu/jobs_data/' + tableName
+
     os.chdir(source)
 
+
+    local = pytz.timezone("America/Chicago")
+
     for filename in os.listdir(source):
-        readIn = open('test_file.txt', 'r')
+        print('New file')
+        readIn = open(filename, 'r')
         next(readIn)
-        converted = []
         for line in readIn:
             row = line.split('|')
-            jobid = int(row[0])
+            jobid = str(row[0])
             user = str(row[1])
             account = str(row[2])
-            start = datetime.strptime(row[3], '%Y-%m-%dT%H:%M:%S')
-            end = datetime.strptime(row[4], '%Y-%m-%dT%H:%M:%S')
-            submit = datetime.strptime(row[5], '%Y-%m-%dT%H:%M:%S')
+
+            # Conversion to UTC for start, end and submit variables
+            local_start = datetime.strptime(row[3], '%Y-%m-%dT%H:%M:%S')
+            local_dt_strt = local.localize(local_start, is_dst=None)
+            start = local_dt_strt.astimezone(pytz.utc)
+
+
+            local_end = datetime.strptime(row[4], '%Y-%m-%dT%H:%M:%S')
+            local_dt_end = local.localize(local_end, is_dst=None)
+            end = local_dt_end.astimezone(pytz.utc)
+
+            local_submit = datetime.strptime(row[5], '%Y-%m-%dT%H:%M:%S')
+            local_dt_submit = local.localize(local_start, is_dst=None)
+            submit = local_dt_submit.astimezone(pytz.utc)
+
             queue = str(row[6])
-            timelimit = str(row[7]) # <- Need to convert properly to datetime object
+
+            raw = str(row[7]) # Raw is the timelimit column
+            dash_position = raw.find('-')
+            if (dash_position == 1):
+                found = []
+                if re.search('\:', raw) is not None:
+                    for i in re.finditer('\:', raw):
+                        found.append(i.start(0))
+                if len(found) == 2:
+                    #print('\nDays-Hours:Minutes:Seconds (D-H:M:S) Format')
+                    #print(raw)
+
+                    temp = re.split('[-]', raw)
+                    day = int(temp[0]) * 1440  # The amount of minutes in a day
+
+                    temp1 = temp[1]
+                    hms = re.split('[:]', temp1)
+
+                    h = int(hms[0]) * 60
+                    m = int(hms[1])
+                    s = int(hms[2]) * 0.0166667
+
+                    timelimit = day + h + m + s
+
+                if len(found) == 1:
+                    #print('\nDays-Hours:Minutes (D-H:M) Format')
+                    #print(raw)
+                    temp = re.split('[-]', raw)
+                    day = int(temp[0]) * 1440  # The amount of minutes in a day
+
+                    temp1 = temp[1]
+                    hms = re.split('[:]', temp1)
+                    h = int(hms[0]) * 60
+                    m = int(hms[1])
+
+                    timelimit = day + h + m
+
+                if re.search('\:', raw) is None:  # Working
+                    #print('\nDay-Hour (DH) Format')
+                    #print(raw)
+                    temp = re.split('[-]', raw)
+                    day = int(temp[0]) * 1440  # The amount of minutes in a day
+                    h = int(temp[1]) * 60
+                    timelimit = day + h
+            elif (dash_position == -1):  # .find() returns a -1 if the search case is not found
+                found = []
+                if re.search('\:', raw) is not None:
+                    for i in re.finditer('\:', raw):
+                        found.append(i.start(0))
+
+                if len(found) == 2:
+                    #print('\nHours:Minutes:Seconds (H:M:S) Format')
+                    #print(raw)
+                    hms = re.split('[:]', raw)
+                    h = int(hms[0]) * 60
+                    m = int(hms[1])
+                    s = int(hms[2]) * 0.0166667
+
+                    timelimit = h + m + s
+
+                if len(found) == 1:
+                    #print('\nMinutes:Seconds (M:S) Format')
+                    #print(raw)
+                    hms = re.split('[:]', raw)
+                    m = int(hms[0])
+                    s = int(hms[1]) * 0.0166667
+
+                    timelimit = m + s
+                    print(timelimit)
+
+                if re.search('\:', raw) is None:  # Working
+                    #print('\nMinute (MM) Format')
+                    #print(raw)
+                    timelimit = int(raw)
+
             jobname = str(row[8])
+
             state = str(row[9]) # <- Need to fix? It's a state, COMPLETED OR FAILED
+
             nnodes = int(row[10])
             reqcpus = int(row[11])
             nodelist = str(row[12])
@@ -88,25 +211,21 @@ def injection(connection, cursor, tableName):
                 'nodelist': nodelist,
             }
 
-            #print('Add data statement: ', add_data)
-
             cursor.execute(add_data, data)
-            connection.commit()
-            #print('Iterate through again') # Need to add statement based on the warning code of whether it was successful or not
 
-
+    # Commit after writing all the data of the current file into the table
+    connection.commit()
+    readIn.close()
+    cursor.close()
 
 def main():
-    tableName = input('Enter a name of a HPC machine you would like to access: ')
-    connection = mysql.connector.connect(host="127.0.0.1", user='costaki', passwd='password', database="HPC_Job_Time_Data")
-    isConnected(connection) # Good
-    cursor = connection.cursor()
 
-    createIfNotExists(connection, cursor, tableName)
-    injection(connection, cursor, tableName)
+    tableName = input('Enter a name of a HPC machine you would like to access: ')
+    connection = connect()
+    createIfNotExists(connection, tableName)
+    injection(connection, tableName)
 
     connection.close()
-    cursor.close()
 
 if __name__ == '__main__':
     main()
